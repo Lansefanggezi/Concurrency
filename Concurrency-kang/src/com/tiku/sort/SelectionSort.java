7ccfba4cd8faf3dbddec96aeacb10d026ca10792
Carl <625380803@qq.com>
2017-09-09 23:11:27 +0800

    Selection sort

--- Concurrency-kang/src/com/tiku/sort/SelectionSort.java
+++ Concurrency-kang/src/com/tiku/sort/SelectionSort.java
@@ -5,7 +5,26 @@ import java.util.Scanner;
 public class SelectionSort {
 	public static void main(String[] args){
 		int[] A = {1,3,5,7,9,2,8,2,4,6};
-		int ;
-		
+		for(int i = 0;i<A.length-1;i++){
+			int max = i;
+			for(int j = i + 1;j<A.length;j++){
+				if(A[j]>A[max]){
+					max = j;
+				}
+			}
+			new SelectionSort().Swap(A, i, max);
+		}
+		new SelectionSort().print(A);
+	}
+	private void Swap(int [] A , int i, int j){
+		int temp ;
+		temp = A[i];
+		A[i] = A[j];
+		A[j] = temp;
+	}
+	private void print(int [] A){
+		for(int i = 0;i<A.length;i++){
+			System.out.println(A[i]);
+		}
 	}
 }
